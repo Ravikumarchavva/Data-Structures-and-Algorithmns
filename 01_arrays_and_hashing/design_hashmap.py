f36b3e3e850c2f9a8408ec48b1f37a215fe295dd
Ravikumar Chavva <chavvaravikumarreddy2004@gmail.com>
2025-09-17 22:41:16 +0530

    Refactor MyHashMap class to include a main block for demonstration and testing

--- 01_arrays_and_hashing/design_hashmap.py
+++ 01_arrays_and_hashing/design_hashmap.py
@@ -31,9 +31,13 @@ class MyHashMap:
 
         
 
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+if __name__ == "__main__":
+    myHashMap = MyHashMap()
+    myHashMap.put(1, 1)          # The map is now [[1,1]]
+    myHashMap.put(2, 2)          # The map is now [[1,1], [2,2]]
+    print(myHashMap.get(1))      # return 1, The map is now [[1,1], [2,2]]
+    print(myHashMap.get(3))      # return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+    myHashMap.put(2, 1)          # The map is now [[1,1], [2,1]] (i.e., update the existing value)
+    print(myHashMap.get(2))      # return 1, The map is now [[1,1], [2,1]]
+    myHashMap.remove(2)          # remove the mapping for 2, The map is now [[1,1]]
+    print(myHashMap.get(2))      # return -1 (i.e., not found), The map is now [[1,1]]
