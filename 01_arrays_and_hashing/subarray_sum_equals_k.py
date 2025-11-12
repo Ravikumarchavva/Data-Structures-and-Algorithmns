60aeed8f3ed3805f8956450798c67ff4e2fc2cd7
Ravikumar Chavva <chavvaravikumarreddy2004@gmail.com>
2025-11-12 23:42:04 +0530

    Add subarraySum method to Solution class for counting subarrays that sum to k

---
+++ 01_arrays_and_hashing/subarray_sum_equals_k.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        prefix_sum = 0
+        prefix_sum_counts = {0: 1}
+
+        for num in nums:
+            prefix_sum += num
+            if (prefix_sum - k) in prefix_sum_counts:
+                count += prefix_sum_counts[prefix_sum - k]
+            prefix_sum_counts[prefix_sum] = prefix_sum_counts.get(prefix_sum, 0) + 1
+    
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.subarraySum([1,1,1], 2))          # Output: 2
+    print(sol.subarraySum([1,2,3], 3))          # Output: 2
+    print(sol.subarraySum([1,-1,0], 0))         # Output: 3
