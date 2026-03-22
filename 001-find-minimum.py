b7dd1a6e140baa305fb38b2bfd81ecd62f49376a
Ravikumar Chavva <chavvaravikumarreddy2004@gmail.com>
2026-03-23 01:16:15 +0530

    001

--- 001-find-minimum.py
+++ 001-find-minimum.py
@@ -1,4 +1,6 @@
 def get_minimum(arr: list) -> int:
+    if len(arr) == 0:
+        return None
     max = float("inf")
     for i in arr:
         if i < max:
@@ -8,3 +10,4 @@ def get_minimum(arr: list) -> int:
 
 if __name__ == '__main__':
     print(get_minimum([1,2,3]))
+    print(get_minimum([]))
